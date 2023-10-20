a9c1e5440f9cca12837c9d6319c0efa37e906617
madeinkm <madeinkm@gmail.com>
2023-10-20 21:58:53 +0900

    10/20학원수업

--- Assets/Scripts/GhostCam.cs
+++ Assets/Scripts/GhostCam.cs
@@ -29,11 +29,31 @@ public class GhostCam : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
-            cam.transform.position += Vector3.forward * camSpeed * Time.deltaTime;
+            //cam.transform.position += Vector3.forward * camSpeed * Time.deltaTime; //월드포지션 기준으로 움직임.
+            //cam.transform.position += cam.transform.rotation * Vector3.forward * camSpeed * Time.deltaTime; //카메라포지션 기준으로 움직임
+            cam.transform.position += cam.transform.TransformDirection(Vector3.forward) * camSpeed * Time.deltaTime; // 위 표현과 동일함
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            cam.transform.position += cam.transform.TransformDirection(Vector3.back) * camSpeed * Time.deltaTime; // 위 표현과 동일함
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            cam.transform.position += cam.transform.TransformDirection(Vector3.left) * camSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            cam.transform.position += cam.transform.TransformDirection(Vector3.right) * camSpeed * Time.deltaTime;
         }
     }
     private void rotating()
     {
+        float mouseX = Input.GetAxisRaw("Mouse X") * mouseSens * Time.deltaTime;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSens * Time.deltaTime;
 
+        rotateValue.x += mouseY * -1;
+        rotateValue.y += mouseX;
+
+        cam.transform.rotation = Quaternion.Euler(rotateValue.x, rotateValue.y, 0f);//(rotateValue) 과 동일
     }
 }
